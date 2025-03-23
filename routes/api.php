3a281ec1b5957eb360d833cfc12cc694036d8d0b
s-nao <s.nao.dev@gmail.com>
2025-03-23 11:05:48 +0900

    tokenの発行と振り分け

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ApiTokenController;
 
-Route::middleware(['throttle:token'])->get('/test', function () {
-    return response()->json(['message' => 'token ok']);
+Route::middleware(['auth.token', 'throttle:token'])->get('/test', function () {
+    return response()->json(['message' => 'ok']);
 });
+
+// routes/api.php
+Route::post('/token', [ApiTokenController::class, 'store']); // トークン発行用
 
